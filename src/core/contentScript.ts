7800c82b61c5580060e4ddc89c87d84f5d862230
Wilson Neto <contato@wilsoneto.com.br>
2025-05-17 17:24:38 -0300

    feat: add description and criteria updates to page

--- src/core/contentScript.ts
+++ src/core/contentScript.ts
@@ -13,6 +13,7 @@ class PbiPage {
     console.log("valueSetter", valueSetter);
     console.log("prototypeValueSetter", prototypeValueSetter);
 
+    element.value = value;
     if (valueSetter && valueSetter !== prototypeValueSetter) {
       prototypeValueSetter?.call(element, value);
     } else {
@@ -32,9 +33,37 @@ class PbiPage {
     console.log("titleElement", titleElement, title);
   }
 
+  private static updateDescription(description: string) {
+    const descriptionElement = document.querySelector('[aria-label="Description"]') as HTMLDivElement;
+    if (!descriptionElement){
+      console.log("descriptionElement not found");
+      return;
+    }
+
+    descriptionElement.innerHTML = description;
+    descriptionElement.classList.toggle("show-placeholder", false);
+    descriptionElement.dispatchEvent(new Event('input', { bubbles: true }));
+    console.log("descriptionElement", descriptionElement, description);
+  }
+
+  private static updateCriteria(criteria: string) {
+    const criteriaElement = document.querySelector('[aria-label="Acceptance Criteria"]') as HTMLDivElement;
+    if (!criteriaElement){
+      console.log("criteriaElement not found");
+      return;
+    }
+
+    criteriaElement.innerHTML = criteria;
+    criteriaElement.classList.toggle("show-placeholder", false);
+    criteriaElement.dispatchEvent(new Event('input', { bubbles: true }));
+    console.log("criteriaElement", criteriaElement, criteria);
+  }
+
   public static updatePage(pbidata: PbiData) {
     console.log("updatePage", pbidata);
     PbiPage.updateTitle(pbidata.title);
+    PbiPage.updateDescription(pbidata.description);
+    PbiPage.updateCriteria(pbidata.criteria);
   }
 }
 
