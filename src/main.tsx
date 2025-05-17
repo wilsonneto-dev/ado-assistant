11d50369d0e1bfcbef4259fb096a36b93e418264
Wilson Neto <contato@wilsoneto.com.br>
2025-05-17 15:25:52 -0300

    chore: migrations and adjusting code structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './shared/theme/theme.scss'
-import App from "./shared/App.tsx";
+import './ui/shared/theme/theme.scss'
+import App from "./ui/shared/App.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
